4e0e0f03b9966d81253d8b50d76345628355ab97
Eloback <eduardolloback@gmail.com>
2021-04-26 11:37:46 -0300

    added files

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+dotenv.config();
+const Connection_URI = 'mongodb+srv://'+process.env.DB_KEY+'@cluster0.jq9b3.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+mongoose.connect(Connection_URI, {useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=> app.listen(PORT, ()=> console.log("Listening in port "+PORT) ))
+    .catch((error)=> console.log(error) );
+
+mongoose.set('useFindAndModify', false);
