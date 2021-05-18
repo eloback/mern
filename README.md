8004f09fe40be2487f1bca3650d719a4ebea7938
Eloback <eduardolloback@gmail.com>
2021-05-18 14:14:44 -0300

    docker-compose with mongodb

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 //client/npm start
 //client/npm start || run dev
 create .env
+
+in server:
+heroku git:remote -a eloback-memories
--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from "body-parser";
-import dotenv from 'dotenv';
-dotenv.config();
-const Connection_URI = 'mongodb+srv://'+process.env.DB_KEY+'@cluster0.jq9b3.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const Connection_URI =  'mongodb://mongo:27017/memories' // Docker Mongo
+//const Connection_URI = 'mongodb://127.0.0.1:27017/memories' //Local Mongo
+//const Connection_URI = 'mongodb+srv://'+process.env.DB_KEY+'@cluster0.jq9b3.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority' // Mongo Atlas
+const PORT = process.env.PORT || 3001;
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/user.js'
 
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
+app.get('/', (req, res)=>res.send("Server is Running"));
 
 mongoose.connect(Connection_URI, {useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=> app.listen(PORT, ()=> console.log("Listening in port "+PORT) ))
